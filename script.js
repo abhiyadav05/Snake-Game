b7b96586a09c864fe943c6df8d99c91e86e02e72
abhiyadav05 <abhshekyadav95065@gmail.com>
2025-08-08 18:51:43 +0530

    write the code to set the position of the snake and read the direction via keyboard in javaScript

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 console.log("helooo")
+
+let snake=document.querySelector("#snake")
+let food=document.querySelector("#food")
+
+var snakePosition={
+    x : 15,
+    y : 10,
+}
+function setPositionOfSnake(){
+        snake.style.gridColumn=snakePosition.x;
+        snake.style.gridRow=snakePosition.y;
+}
+// This is all the function of moving the snake
+
+function upPressed(){
+       snakePosition.y--;
+        setPositionOfSnake();
+
+}
+function leftPressed(){
+
+}
+function rightPressed(){
+
+}
+function downPressed(){
+
+}
+
+// This is the function of reading the key
+function startFunctionOfKey(){
+    document.addEventListener("keydown",(event) =>{
+        if(event.key==="ArrowUp"){
+           upPressed();
+        }
+         if(event.key==="ArrowDown"){
+            downPressed();
+        }
+         if(event.key==="ArrowLeft"){
+            leftPressed();
+        }
+        if(event.key==="ArrowRigh"){
+            rightPressed();
+        }
+    })
+}
+
+// This is the function of start the game
+
+function gameStart(){
+    for(let i=snakePosition.x;i<30;i++){
+       setTimeout(()=>{
+         snake.style.gridColumn=i;
+       },(i - snakePosition.x) * 500)
+    }
+    startFunctionOfKey();
+}
+setPositionOfSnake();
+gameStart()
